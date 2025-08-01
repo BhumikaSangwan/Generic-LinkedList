17fc6c4acd25e4c4c85d6b440919300ee43331b3
Bhumika <bhumikasangwan136@gmail.com>
2025-08-01 18:30:52 +0530

    generic Hash Table

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,25 @@
 int main()
 {
 
-    LinkedList<int> list;
-    list.append(1);
-    list.append(2);
-    list.append(3);
+    // LinkedList<int> list;
+    // list.append(1);
+    // list.append(2);
+    // list.append(3);
 
+    // list.print();
+
+    // list.prepend(0);
+    // list.print();
+
+    LinkedList<std::string> list;
+    list.append("Hello");
+    list.append("World");
     list.print();
-
-    list.prepend(0);
+    list.prepend("Hi");
+    list.insert("!", 2);
+    list.insert("abc", 1);
+    list.print();
+    list.removeFromIndex(1);
     list.print();
 
     std::cout << "\nDone";
